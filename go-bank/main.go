0c142f9cc16d5cc75dd0ef5ff86832eb1c3cbf64
Mehmet <sumer682000@gmail.com>
2023-07-10 21:59:51 +0300

    jwt auth implemented

--- go-bank/main.go
+++ go-bank/main.go
@@ -1,6 +1,49 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"log"
+)
+
+func seedAccount(store Storage, fname, lname, pw string) *Account {
+	acc, err := NewAccount(fname, lname, pw)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := store.CreateAccount(acc); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("new account", acc.Number)
+
+	return acc
+}
+
+func seedAccounts(s Storage) {
+	seedAccount(s, "a", "b", "hunter8888")
+}
+
 func main() {
-	server := NewAPIServer(":3000")
+	seed := flag.Bool("seed", false, "seed the db")
+	flag.Parse()
+
+	store, err := NewPostgresStore()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = store.Init()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if *seed {
+		fmt.Println("seeding the database")
+		seedAccounts(store)
+	}
+
+	server := NewAPIServer(":3000", store)
 	server.Run()
 }
--- go-bank/types.go
+++ go-bank/types.go
@@ -1,21 +1,47 @@
 package main
 
-import "math/rand"
+import (
+	"math/rand"
+	"time"
 
-type Account struct {
-	ID        int    `json:"id"`
-	FirstName string `json:"firstName"`
-	LastName  string `json:"lastName"`
-	Number    int64  `json:"number"`
-	Balance   int64  `json:"balance"`
+	"golang.org/x/crypto/bcrypt"
+)
+
+type LoginRequest struct {
+	Number   int64  `json:"number"`
+	Password string `json:"password"`
+}
+type TransferRequest struct {
+	ToAccount int `json:"toAccount"`
+	Amount    int `json:"amount"`
 }
 
-func NewAccount(firstName, lastName string) *Account {
-	return &Account{
-		ID:        rand.Intn(1000),
-		FirstName: firstName,
-		LastName:  lastName,
-		Number:    int64(rand.Intn(100000)),
-		Balance:   0,
+type CreateAccountRequest struct {
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+	Password  string `json:"password"`
+}
+
+type Account struct {
+	ID                int       `json:"id"`
+	FirstName         string    `json:"firstName"`
+	LastName          string    `json:"lastName"`
+	Number            int64     `json:"number"`
+	EncryptedPassword string    `json:"-"`
+	Balance           int64     `json:"balance"`
+	CreatedAt         time.Time `json:"createdAt"`
+}
+
+func NewAccount(firstName, lastName, password string) (*Account, error) {
+	encpw, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, err
 	}
+	return &Account{
+		FirstName:         firstName,
+		LastName:          lastName,
+		EncryptedPassword: string(encpw),
+		Number:            int64(rand.Intn(100000)),
+		CreatedAt:         time.Now().UTC(),
+	}, nil
 }
